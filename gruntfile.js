92566954c91a8a72ccb53272f350ba64ab33897f
Fredrick Brennan <admin@8chan.co>
2015-03-22 12:30:24 +0800

    Simpler gruntfile without annoying jshint

--- gruntfile.js
+++ gruntfile.js
@@ -1,90 +1,41 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      options: {
-        bitwise: true,
-        camelcase: true,
-        indent: 2,
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        white: true,
-        validthis: true,
-        quotmark: 'single',
-        globals: {
-          'window': true,
-          'jQuery': true,
-          'document': true,
-          'Image': true,
-          'setTimeout': true,
-          'clearTimeout': true,
-          'event': true,
-          'CanvasRenderingContext2D': true
-        }
-      },
-      files: {
-        src: ['./plugins/**/src/wPaint-*.js', './src/*.js']
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
-      },
-      my_target: {
-        files: {
-          './wPaint.min.js': ['./src/wPaint.js', './src/wPaint.utils.js'],
-          './plugins/main/wPaint.menu.main.min.js': ['./plugins/main/src/wPaint.menu.main.js', './plugins/main/src/fillArea.min.js'],
-          './plugins/text/wPaint.menu.text.min.js': ['./plugins/text/src/wPaint.menu.text.js'],
-          './plugins/shapes/wPaint.menu.main.shapes.min.js': ['./plugins/shapes/src/wPaint.menu.main.shapes.js', './plugins/shapes/src/shapes.min.js'],
-          './plugins/file/wPaint.menu.main.file.min.js': ['./plugins/file/src/wPaint.menu.main.file.js']
-       }
-      }
-    },
-    stylus: {
+    /*stylus: {
       compile: {
         options: {
           import: ['nib', '../lib/mixins'],
         },
         files: {
-          './wPaint.min.css': './src/wPaint.css'
+          './wPaint.min.css': ['./lib/wColorPicker.min.css', './src/wPaint.css']
         }
       }
-    },
+    },*/
     concat: {
-      basic_and_extras: {
+      my_target: {
         files: {
-          'wPaint-min.js': ['./lib/wColorPicker.min.js', './wPaint.min.js'],
-          'wPaint-min.css': ['./lib/wColorPicker.min.css', './wPaint.min.css'],
-        },
+          './wPaint.min.js': ['./lib/rgbHex.js', './lib/keymaster.js', './lib/wColorPicker.js', './src/wPaint.utils.js', './src/wPaint.js'],
+          './plugins/main/main.js': ['./plugins/main/src/wPaint.menu.main.js', './plugins/main/src/fillArea.min.js'],
+          './plugins/text/text.js': ['./plugins/text/src/wPaint.menu.text.js'],
+          './plugins/shapes/shapes.js': ['./plugins/shapes/src/wPaint.menu.main.shapes.js', './plugins/shapes/src/shapes.min.js'],
+          './lib/plugins.js': ['./plugins/main/main.js', './plugins/text/text.js', './plugins/shapes/shapes.js'],
+          './2ch.js': ['./locale/ja_JP.js', './lib/gettext.js', './lib/jquery.1.10.2.min.js', './lib/jquery-ui.custom.min.js', './wPaint.min.js', './lib/plugins.js', './src/oekaki8.js'],
+          './8ch.js': ['./wPaint.min.js', './lib/plugins.js'],
+       }
       }
     },
-    watch: {
-      files: [
-        './src/wPaint.css',
-        './src/wPaint.js',
-        './plugins/file/src/wPaint.menu.main.js',
-        './plugins/file/src/wPaint.menu.text.js',
-        './plugins/file/src/wPaint.menu.main.shapes.js',
-        './plugins/file/src/wPaint.menu.main.file.js'
-      ],
-      tasks: ['uglify']
+    uglify: {
+      my_target: {
+        files: {
+          './oekaki.js': ['2ch.js'],
+       }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['jshint', 'stylus', 'uglify']);
+  grunt.registerTask('default', ['stylus', 'concat', 'uglify']);
 };
